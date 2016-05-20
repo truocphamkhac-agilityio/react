5c84f599dcedefa2d73f383588825a6fa5ec0731
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-05-20 16:04:03 +0700

    Basic display component based on start webapp by webpack.

---
+++ app/components/H1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Title from '../../src/components/Title';
+
+class H1 extends Component {
+
+  render() {
+    return(
+       <Title />
+    );
+  }
+
+}
+module.exports = H1;
---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import H1 from './components/H1';
+
+ReactDOM.render(
+  <H1 />,
+  document.getElementById('main')
+);
