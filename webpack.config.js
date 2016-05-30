2572e32b950799e341be14db6763b83419143969
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2016-05-30 11:17:17 +0700

    Optimize bundle.js file when run command: `npm run build`

--- webpack.config.js
+++ webpack.config.js
@@ -84,5 +84,13 @@ if (TARGET === 'start' || !TARGET) {
 }
 
 if (TARGET === 'build') {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
